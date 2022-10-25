5ce8a21460c380bebfe6dca959fc33bae0d8c417
reuslok <reuslok@gmail.com>
2022-10-25 09:59:54 +0200

    second commit

--- src/main/java/edu/es/eoi/Address.java
+++ src/main/java/edu/es/eoi/Address.java
@@ -1,5 +1,51 @@
 package edu.es.eoi;
 
 public class Address {
-
+	
+	private int id;
+	private String name;
+	private String adress;
+	private int number;
+	private String apartment;
+	private int zipCode;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getApartment() {
+		return apartment;
+	}
+	public void setApartment(String apartment) {
+		this.apartment = apartment;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	
 }
